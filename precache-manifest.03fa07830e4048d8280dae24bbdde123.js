001e9fcad52d407877e3f7edcea34684caba8515
Jessica Van Brummelen <jess.vanbrummelen@gmail.com>
2020-04-15 22:58:01 -0700

    Updates

---
+++ precache-manifest.03fa07830e4048d8280dae24bbdde123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5cc3c2e28d5468f852ecce94dd6b76",
+    "url": "/jeopardy/index.html"
+  },
+  {
+    "revision": "c5c5dc94c1e2d598dd03",
+    "url": "/jeopardy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a4a948bb6066b1d6b67a",
+    "url": "/jeopardy/static/js/2.5d48c2b6.chunk.js"
+  },
+  {
+    "revision": "260254c2b55f2376c2a0340f77dec2ca",
+    "url": "/jeopardy/static/js/2.5d48c2b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5c5dc94c1e2d598dd03",
+    "url": "/jeopardy/static/js/main.7ae3746f.chunk.js"
+  },
+  {
+    "revision": "060a994d4f2614f41faa",
+    "url": "/jeopardy/static/js/runtime-main.0adbcc07.js"
+  }
+]);
